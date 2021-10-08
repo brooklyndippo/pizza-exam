e4693fc68d4df0ef52f8c315eb2ab757dd73398d
Brooklyn Dippo <brooklyndippo@gmail.com>
2021-10-08 10:40:06 -1000

    initialized repo

---
+++ scripts.js
@@ -0,0 +1,28 @@
+//-----------------------------------------------
+// SELECT INPUTS/OUTPUTS FROM HTML DOC
+
+const item = document.querySelector('#id')
+
+
+
+//-----------------------------------------------
+// ADD EVENT LISTENERS
+
+item.addEventListener('change', functionName)
+
+
+//-----------------------------------------------
+// FUNCTION TO DO THE THING
+
+
+
+
+//-----------------------------------------------
+// CONSOLE LOG TO CHECK THE THING
+
+
+
+
+
+//-----------------------------------------------
+// PUSH OUTPUT VALUES TO DISPLAY
