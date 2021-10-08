2e2e4097870d38335d85be96a0db5380c75cc6f1
Brooklyn Dippo <brooklyndippo@gmail.com>
2021-10-08 11:27:43 -1000

    basic CSS, added querySelectors, event listeners, and proved function running

--- scripts.js
+++ scripts.js
@@ -1,20 +1,52 @@
 //-----------------------------------------------
 // SELECT INPUTS/OUTPUTS FROM HTML DOC
 
-const item = document.querySelector('#id')
+//const item = document.querySelector('#id')
+
+// Define variables for three sections
+const input = document.querySelector('#input')
+const output = document.querySelector('#output')
+const visual = document.querySelector('#visual')
+
+// Define varibales for input, range, and selector
+const fnameInput = document.querySelector('#name')
+const diameterInput = document.querySelector('#diameter')
+const toppingsInput = document.querySelector('#toppings')
+
+// Define variables for the display output
+const nameOutput = document.querySelector('#name-output')
+const diameterOutput = document.querySelector('#diameter-output')
+const typeOutput = document.querySelector('#type-output')
+const costOutput = document.querySelector('#cost-output')
+
+
+// Define variable for the visualizer
+const pizza = document.querySelector('#pizza')
 
 
 
 //-----------------------------------------------
 // ADD EVENT LISTENERS
 
-item.addEventListener('change', functionName)
+//item.addEventListener('change', functionName)
+
+fnameInput.addEventListener('change', handlePizza) 
+diameterInput.addEventListener('change', handlePizza)
+toppingsInput.addEventListener('change', handlePizza)
 
 
 //-----------------------------------------------
-// FUNCTION TO DO THE THING
+// FUNCTION TO HANDLE PIZZA MAKING!
+
+function handlePizza(e) {
+    const fname = fnameInput.value
+    const diameter = parseInt(diameterInput.value)
+    const toppings = toppingsInput.value
+
+    console.log (fname, diameter, toppings)
 
 
+}
 
 
 //-----------------------------------------------
